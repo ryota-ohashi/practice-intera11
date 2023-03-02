a14b0a157ab5aa1d08354ded05c981e83a77e797
Ryota Ohhashi <ryota.ohhashi@accenture.com>
2023-03-02 21:58:38 +0900

    modified

--- src/js/main.ts
+++ src/js/main.ts
@@ -7,6 +7,8 @@ const sketch = (p: p5) => {
     order: number,
     wordIndex: number,
     letter: string,
+    initX: number,
+    initY: number,
     x: number,
     y: number,
     vx: number,
@@ -36,8 +38,8 @@ const sketch = (p: p5) => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     word.map((words, order) => {
       splitWord(words).map((letter, index) => {
-        // console.log(initPos(letter, index));
-        wordArray.push(initPos(letter, order, index));
+        // console.log(init(letter, index));
+        wordArray.push(init(letter, order, index));
       });
     });
     // console.log(wordArray);
@@ -63,24 +65,24 @@ const sketch = (p: p5) => {
       let totalLength = filteredCollectedArray.length;
 
       obj.x = p.lerp(obj.x, p.width / 2, 0.05);
-      obj.y = p.lerp(obj.y, p.map(index, 0, totalLength, p.height / 4, p.height * 3 / 4), 0.05);
+      obj.y = p.lerp(obj.y, p.map(index, 0, totalLength, p.height / 5, p.height * 4 / 5), 0.05);
       p.text(obj.letter, obj.x, obj.y);
     });
 
     // 中央に集めない文字の処理
-    let filteredUnCorrectedArray = wordArray.filter(obj => obj.order !== currentNumber);
+    let filteredUnCollectedArray = wordArray.filter(obj => obj.order !== currentNumber);
 
-    filteredUnCorrectedArray.forEach(obj => {
+    filteredUnCollectedArray.forEach(obj => {
 
       if (obj.x > (p.width / 2 - 50) && obj.x < (p.width / 2 + 50)) {
-
-
+        obj.x = obj.initX;
+        obj.y = obj.initY;
       }else{
         obj.vx = 2 * p.noise(obj.x / 100, obj.y / 100, t / 100) - 1;
         obj.vy = 2 * p.noise(obj.x / 100, obj.y / 100, (t + 1000) / 100) - 1;
+        obj.x += obj.vx;
+        obj.y += obj.vy;
       }
-      obj.x += obj.vx;
-      obj.y += obj.vy;
       p.text(obj.letter, obj.x, obj.y);
     });
 
@@ -91,17 +93,51 @@ const sketch = (p: p5) => {
   };
 
   // const randomMinusOneOrOne = () => {
-  //   const value = Math.ceil(Math.sin(Math.random() * Math.PI * 2));
-  //   return value > 0 ? 1 : -1;
+  //   const value = p.random(0,1);
+  //   return value > 0.5 ? 1 : -1;
   // }
 
-  const initPos = (letter: string, order: number, wordIndex: number) => {
+  // const createPositionX = ():number => {
+  //   let flagX = false;
+  //   let positionX;
+  //   while (flagX === false) {
+  //     positionX = p.random(p.width);
+  //     if (positionX < (p.width / 2 - 50) && positionX > (p.width / 2 + 50)) flagX = true;
+  //   }
+  //   return Number(positionX);
+  // }
+
+  // const createPositionY = ():number => {
+  //   let flagY = false;
+  //   let positionY;
+  //   while (flagY === false) {
+  //     positionY = p.random(p.height);
+  //     if (positionY < (p.height / 4) && positionY > (p.height * 3 / 4)) flagY = true;
+  //   }
+  //   return Number(positionY);
+  // }
+
+  const init = (letter: string, order: number, wordIndex: number) => {
+
+    // let flagX = false;
+    // let positionX;
+    // while (flagX === false) {
+    //   positionX = p.random(p.width);
+    //   if (positionX < (p.width / 2 - 50) && positionX > (p.width / 2 + 50)) flagX = true;
+    // }
+
     const wordPos: Status = {
       order: order,
       wordIndex: wordIndex,
       letter: letter,
-      x: p.random(p.windowWidth),
-      y: p.random(p.windowHeight),
+      // initX: createPositionX(),
+      // initY: createPositionY(),
+      // x: createPositionX(),
+      // y: createPositionY(),
+      initX: p.random(p.width),
+      initY: p.random(p.height),
+      x: p.random(p.width),
+      y: p.random(p.height),
       vx: 0,
       vy: 0
     };
