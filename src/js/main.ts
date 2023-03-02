b42eddd032a46978c49164d3ba64202b6bab650f
ryota ohashi <ryota.812.skt@gmail.com>
2023-03-03 00:08:34 +0900

    modified

--- src/js/main.ts
+++ src/js/main.ts
@@ -11,11 +11,11 @@ const sketch = (p: p5) => {
     initY: number,
     x: number,
     y: number,
-    vx: number,
-    vy: number
+    t: number
   }
 
   let t = 1;
+  let interval = 600;
   let textSize = 24;
   let myFont: p5.Font;
   let fontPath = "/assets/fonts/YujiBoku-Regular.ttf";
@@ -27,7 +27,6 @@ const sketch = (p: p5) => {
   ];
   const wordLength: number = word.length;
   let currentNumber:number = 0;
-
   let wordArray: Status[] = [];
 
   p.preload = () => {
@@ -37,12 +36,12 @@ const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     word.map((words, order) => {
+
       splitWord(words).map((letter, index) => {
-        // console.log(init(letter, index));
         wordArray.push(init(letter, order, index));
       });
+
     });
-    // console.log(wordArray);
 
   };
 
@@ -53,20 +52,23 @@ const sketch = (p: p5) => {
     p.textFont(myFont);
     p.fill(0);
 
-    // 600フレームごとにcurrentNumberを変更
-    if (t % 600 === 0 ) currentNumber++;
+    // intervalごとにcurrentNumberを変更
+    if (t % interval === 0 ) currentNumber++;
     if(currentNumber === wordLength) currentNumber = 0;
 
     // 中央に集める文字の処理
     let filteredCollectedArray = wordArray.filter(obj => obj.order === currentNumber);
 
     filteredCollectedArray.forEach(obj => {
+
       let index = obj.wordIndex;
       let totalLength = filteredCollectedArray.length;
 
+      obj.t = 0;
       obj.x = p.lerp(obj.x, p.width / 2, 0.05);
       obj.y = p.lerp(obj.y, p.map(index, 0, totalLength, p.height / 5, p.height * 4 / 5), 0.05);
       p.text(obj.letter, obj.x, obj.y);
+
     });
 
     // 中央に集めない文字の処理
@@ -74,16 +76,16 @@ const sketch = (p: p5) => {
 
     filteredUnCollectedArray.forEach(obj => {
 
-      if (obj.x > (p.width / 2 - 50) && obj.x < (p.width / 2 + 50)) {
-        obj.x = obj.initX;
-        obj.y = obj.initY;
+      if (obj.t < 90) {
+        obj.t++;
+        obj.x += (obj.initX - obj.x) * 0.05;
+        obj.y += (obj.initY - obj.y) * 0.05;
       }else{
-        obj.vx = 2 * p.noise(obj.x / 100, obj.y / 100, t / 100) - 1;
-        obj.vy = 2 * p.noise(obj.x / 100, obj.y / 100, (t + 1000) / 100) - 1;
-        obj.x += obj.vx;
-        obj.y += obj.vy;
+        obj.x += 2 * p.noise(obj.x / 100, obj.y / 100, t / 100) - 1;
+        obj.y += 2 * p.noise(obj.x / 100, obj.y / 100, (t + 1000) / 100) - 1;
       }
       p.text(obj.letter, obj.x, obj.y);
+
     });
 
   };
@@ -92,54 +94,34 @@ const sketch = (p: p5) => {
     return word.split("");
   };
 
-  // const randomMinusOneOrOne = () => {
-  //   const value = p.random(0,1);
-  //   return value > 0.5 ? 1 : -1;
-  // }
+  const createPositionX = (): number => {
 
-  // const createPositionX = ():number => {
-  //   let flagX = false;
-  //   let positionX;
-  //   while (flagX === false) {
-  //     positionX = p.random(p.width);
-  //     if (positionX < (p.width / 2 - 50) && positionX > (p.width / 2 + 50)) flagX = true;
-  //   }
-  //   return Number(positionX);
-  // }
+    let positionX;
+    const flag = p.random(1);
+    if (flag > 0.5) {
+      positionX = p.random(p.width / 2 - 120);
+    }else{
+      positionX = p.random(p.width / 2 - 120, p.width);
+    }
+    return positionX;
+  }
 
-  // const createPositionY = ():number => {
-  //   let flagY = false;
-  //   let positionY;
-  //   while (flagY === false) {
-  //     positionY = p.random(p.height);
-  //     if (positionY < (p.height / 4) && positionY > (p.height * 3 / 4)) flagY = true;
-  //   }
-  //   return Number(positionY);
-  // }
+  const createPositionY = (): number => {
+    return p.random(p.height);
+  }
+
 
   const init = (letter: string, order: number, wordIndex: number) => {
-
-    // let flagX = false;
-    // let positionX;
-    // while (flagX === false) {
-    //   positionX = p.random(p.width);
-    //   if (positionX < (p.width / 2 - 50) && positionX > (p.width / 2 + 50)) flagX = true;
-    // }
 
     const wordPos: Status = {
       order: order,
       wordIndex: wordIndex,
       letter: letter,
-      // initX: createPositionX(),
-      // initY: createPositionY(),
-      // x: createPositionX(),
-      // y: createPositionY(),
-      initX: p.random(p.width),
-      initY: p.random(p.height),
-      x: p.random(p.width),
-      y: p.random(p.height),
-      vx: 0,
-      vy: 0
+      initX: createPositionX(),
+      initY: createPositionY(),
+      x: createPositionX(),
+      y: createPositionY(),
+      t: 0
     };
     return wordPos;
   };
